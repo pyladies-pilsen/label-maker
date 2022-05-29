b3c7bad4d2312e9bab300fdc6d8db8ac9ee6d91b
Jakub Cervinka <jakub.cervinka@zf.com>
2022-05-29 13:10:43 +0200

    output to word template
    rows replacement,
    empty space on page is truly empty

--- calc.py
+++ calc.py
@@ -1,5 +1,88 @@
+import logging
+from functools import reduce
+from itertools import zip_longest
+from operator import or_
+
+LABELS_PER_PAGE = 32
+
+log = logging.getLogger(__name__)
+
+
 def calculate_unit_price(data):
     """Calculate price per unit."""
+    log.debug('claculating unit price')
     for item in data:
         item['unit_price'] = round(item['total_price'] / item['quantity'], 2)
+    log.debug('done')
     return data
+
+
+def split_to_page_size(data):
+    """split data to chunks based on labels per page"""
+    log.debug('splitting data to pages')
+    # prepare fill dict with empty values
+    # NOTE: this is so the rest of the page fills as blank and is not printed
+    fill_value = {k: '' for k in data[0]}
+    args = [iter(data)] * LABELS_PER_PAGE
+    log.debug('done')
+    return zip_longest(*args, fillvalue=fill_value)
+
+
+def enumerate_keys(page_data):
+    """Edit keys so they can be used in replacement.
+
+    name -> name_1, name_2 ...
+    """
+    log.debug('enumerating keys')
+    result = []
+    for num, item in enumerate(page_data, start=1):
+        renamed = {}
+        for key, value in item.items():
+            renamed[f'{key}_{num}'] = value
+        result.append(renamed)
+    log.debug('done')
+    return result
+
+
+def merge_page_data(data):
+    """Merge list of dictionaries into one."""
+    log.debug('merging page data to a single dictionary')
+    data = reduce(or_, data)
+    log.debug('done')
+    return data
+
+
+def round_all_values(dictionary):
+    """Round all numeric values in a dictionary."""
+    log.debug('rounding numeric values')
+    new_dict = {}
+    for k, v in dictionary.items():
+        try:
+            new_dict[k] = int(round(v, 0))
+        except TypeError:
+            new_dict[k] = v
+    log.debug('done')
+    return new_dict
+
+
+def prepare_rows(data):
+    """Reformat the dicts for replacing.
+
+    We need keys "top_row" "price" "bottom_row"
+    """
+    log.debug('preparing rows')
+    new_data = []
+    for dct in data:
+        dct = round_all_values(dct)
+        top_row = '{name} {form} {quantity} {unit}'.format(**dct)
+        price = '{total_price},-'.format(**dct)
+        bottom_row = '1 {unit} = {unit_price} Kč'.format(**dct)
+        new_data.append(
+            {
+                'top_row'   : top_row,
+                'price'     : price,
+                'bottom_row': bottom_row,
+                }
+            )
+    log.debug('done')
+    return new_data
--- outputs.py
+++ outputs.py
@@ -1,4 +1,15 @@
 import logging
+from pathlib import Path
+
+from docxtpl import DocxTemplate
+
+from calc import enumerate_keys
+from calc import merge_page_data
+from calc import prepare_rows
+from calc import split_to_page_size
+
+WORD_OUTPUT_NAME = 'generated_labels'
+OUTPUT_FOLDER = 'output'
 
 log = logging.getLogger(__name__)
 
@@ -35,3 +46,36 @@ def to_console(data):
 
         filled = template.substitute(sub)
         print(filled)
+
+
+def to_word(data, template):
+    """Output the label data to word files."""
+    log.info('output to word started')
+
+    # prepare output folder
+    Path(OUTPUT_FOLDER).mkdir(exist_ok=True)
+
+    # reformat the dicts for replace
+    data = prepare_rows(data)
+
+    # prepare paginated data
+    page_splitted_data = split_to_page_size(data)
+
+    # for each page
+    for page_num, page_data in enumerate(page_splitted_data, start=1):
+        # open new template for each page
+        doc = DocxTemplate(template)
+
+        # prepare the numbers for replacing
+        replace_ready = enumerate_keys(page_data)
+        context = merge_page_data(replace_ready)
+        # make the replacement
+        log.debug(f'replacing page {page_num}')
+        doc.render(context=context)
+
+        # save to new file
+        filename = f'{page_num:02}_{WORD_OUTPUT_NAME}.docx'
+        log.debug(f'saving to file {filename}')
+        doc.save(Path(OUTPUT_FOLDER) / filename)
+
+    log.info('output to word finished')
