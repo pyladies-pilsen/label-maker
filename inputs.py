43559023358f9545dc5c54026003dbb744ddc764
Jakub Cervinka <jakub.cervinka@zf.com>
2022-05-31 21:59:32 +0200

    better user input with valid options

--- inputs.py
+++ inputs.py
@@ -24,6 +24,54 @@ Example of output:
 
 """
 
+VALID_FORMS = [
+    'gtt',
+    'sir',
+    'sol',
+    'pst',
+    'tbl',
+    'cps',
+    'ung',
+    'crm',
+    'supp',
+    'spr',
+    'grg',
+    ]
+VALID_UNITS = [
+    'g',
+    'ml',
+    ]
+
+
+def positive_float(string):
+    num = float(string)
+    if not 0 < num < 10_000:
+        raise ValueError('Smí být jen od 0 do 10 000.')
+    return num
+
+
+def better_input(question, value_type, valid_options=None):
+    """Validate input based on the passed value type.
+
+    Upon error continuously prompt user for correct answer.
+    """
+
+    while True:
+        try:
+            answer = value_type(input(f'{question}{valid_options if valid_options is not None else ""}: '))
+
+            if valid_options and answer not in valid_options:
+                raise ValueError('neni z povolených hodnot')
+
+            if not answer:
+                raise ValueError('Nepovolujeme prázné hodnoty a nuly.')
+
+        except ValueError:
+            print('Špatně zadaná hodnota')
+
+        else:
+            return answer
+
 
 def user_input():
     """Take input from user.
@@ -40,11 +88,11 @@ def user_input():
     entries = []
     while True:
         item = {
-            'name'       : input('Název: '),
-            'form'       : input('Forma: '),
-            'unit'       : input('Jednotky: '),
-            'quantity'   : input('Počet: '),
-            'total_price': input('Celková cena [Kč]: '),
+            'name'       : better_input('Název', str),
+            'form'       : better_input('Forma', str, valid_options=VALID_FORMS),
+            'unit'       : better_input('Jednotky', str, valid_options=VALID_UNITS),
+            'quantity'   : better_input('Počet', int),
+            'total_price': better_input('Celková cena', positive_float),
             }
         entries.append(item)
 
